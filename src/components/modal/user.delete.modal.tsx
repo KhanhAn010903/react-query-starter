dd4d2cfa87bd2e7fac381245389fe59b47f115fd
Khanhan010903 <quangan010903@gmail.com>
2025-07-24 15:52:30 +0700

    Completed crud user with react query

--- src/components/modal/user.delete.modal.tsx
+++ src/components/modal/user.delete.modal.tsx
@@ -1,11 +1,31 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Spinner } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { toast } from 'react-toastify';
 
 const UserDeleteModal = (props: any) => {
+    const queryClient = useQueryClient();
     const { dataUser, isOpenDeleteModal, setIsOpenDeleteModal } = props;
-
+    const mutation = useMutation({
+        mutationFn: async (payload: any) => {
+            await fetch(`http://localhost:8000/users/${payload.id}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": " application/json"
+                }
+            });
+        },
+        onSuccess(data, variables, context) {
+            toast('Wow so easy! Create succeed');
+            setIsOpenDeleteModal(false)
+            queryClient.invalidateQueries({ queryKey: ['fetchUser'] })
+        },
+    })
     const handleSubmit = () => {
-        console.log({ id: dataUser?.id });
+        mutation.mutate({
+            id: dataUser?.id
+        })
     }
 
     return (
@@ -24,12 +44,25 @@ const UserDeleteModal = (props: any) => {
             <Modal.Body>
                 Delete the user: {dataUser?.email ?? ""}
             </Modal.Body>
-            <Modal.Footer>
-                <Button
-                    variant='warning'
-                    onClick={() => setIsOpenDeleteModal(false)} className='mr-2'>Cancel</Button>
-                <Button onClick={() => handleSubmit()}>Confirm</Button>
-            </Modal.Footer>
+           <Modal.Footer>
+                    {!mutation.isPending ?
+                        <>
+                            <Button
+                                variant='warning'
+                                onClick={() => setIsOpenDeleteModal(false)} className='mr-2'>Cancel</Button>
+                            <Button onClick={() => handleSubmit()}>Save</Button>
+                        </>
+                        :
+                        <Button variant='primary' disabled>
+                            <Spinner
+                                as="span"
+                                animation='border'
+                                size='sm'
+                                role='status'
+                                aria-hidden='true'
+                            /><></> Saving...
+                        </Button>}
+                </Modal.Footer>
         </Modal>
     )
 }
--- src/components/modal/user.edit.modal.tsx
+++ src/components/modal/user.edit.modal.tsx
@@ -3,13 +3,42 @@ import Modal from 'react-bootstrap/Modal';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { useState, useEffect } from 'react';
-
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import { Spinner } from 'react-bootstrap';
+interface IUserUpdate {
+    id: number;
+    name: string;
+    email: string;
+}
 const UserEditModal = (props: any) => {
+    const queryClient = useQueryClient();
     const { isOpenUpdateModal, setIsOpenUpdateModal, dataUser } = props;
     const [id, setId] = useState();
 
     const [email, setEmail] = useState<string>("");
     const [name, setName] = useState<string>("");
+    const mutation = useMutation({
+        mutationFn: async (payload: any) => {
+            const res = await fetch(`http://localhost:8000/users/${payload.id}`, {
+                method: "PUT",
+                body: JSON.stringify({
+                    email: payload.email,
+                    name: payload.name,
+                }),
+                headers: {
+                    "Content-Type": " application/json"
+                }
+            });
+            return res.json();
+        },
+        onSuccess(data, variables, context) {
+            toast('Wow so easy! Create succeed');
+            setIsOpenUpdateModal(false)
+            setEmail(""); setName("")
+            queryClient.invalidateQueries({ queryKey: ['fetchUser'] })
+        },
+    })
 
     useEffect(() => {
         if (dataUser?.id) {
@@ -29,7 +58,9 @@ const UserEditModal = (props: any) => {
             alert("name empty");
             return;
         }
-        console.log({ email, name, id })
+        mutation.mutate({
+            email, name, id
+        })
     }
 
     return (
@@ -66,10 +97,23 @@ const UserEditModal = (props: any) => {
                     </FloatingLabel>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button
-                        variant='warning'
-                        onClick={() => setIsOpenUpdateModal(false)} className='mr-2'>Cancel</Button>
-                    <Button onClick={() => handleSubmit()}>Confirm</Button>
+                    {!mutation.isPending ?
+                        <>
+                            <Button
+                                variant='warning'
+                                onClick={() => setIsOpenUpdateModal(false)} className='mr-2'>Cancel</Button>
+                            <Button onClick={() => handleSubmit()}>Save</Button>
+                        </>
+                        :
+                        <Button variant='primary' disabled>
+                            <Spinner
+                                as="span"
+                                animation='border'
+                                size='sm'
+                                role='status'
+                                aria-hidden='true'
+                            /><></> Saving...
+                        </Button>}
                 </Modal.Footer>
             </Modal>
         </>
